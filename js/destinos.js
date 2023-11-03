82956c9322ca3cf9c222dee596c63ed6c7f46a09
Ezequiel8989 <exequiel.sp.gm@gmail.com>
2023-11-03 09:11:30 -0300

    se agrego el js de destinos

---
+++ js/destinos.js
@@ -0,0 +1,50 @@
+const ciudades = [
+    {
+        id: 1,
+        imagen:"",
+        nombre: "Mar del Plata",
+        precio: "", 
+    },
+    {
+        id: 2,
+        imagen:"",
+        nombre: "San Bernardo",
+        precio: "", 
+    },
+    {
+        id: 3,
+        imagen:"",
+        nombre: "San Luis",
+        precio: "", 
+    },
+    {
+        id: 4,
+        imagen:"",
+        nombre: "Salta",
+        precio: "", 
+    },
+    {
+        id: 5,
+        imagen:"",
+        nombre: "Misiones",
+        precio: "", 
+    },
+    {
+        id: 6,
+        imagen:"",
+        nombre: "Bariloche",
+        precio: "", 
+    },
+    {
+        id: 7,
+        imagen:"",
+        nombre: "Puerto Madryn",
+        precio: "", 
+    },
+    {
+        id: 8,
+        imagen:"",
+        nombre: "Ushuaia",
+        precio: "", 
+    },
+];
