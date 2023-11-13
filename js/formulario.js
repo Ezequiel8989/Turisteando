c813b1e26a08def6ba25f1e29eb927e421e9319e
Ezequiel8989 <exequiel.sp.gm@gmail.com>
2023-11-13 11:53:52 -0300

    este es un proceso de cambio hacia el otro sistema de carga de datos y subirlos al formulario

---
+++ js/formulario.js
@@ -0,0 +1,28 @@
+import destino from "./index.js" ;
+
+// Obtén una referencia al formulario
+const pantallaDestino = document.querySelector('form');
+var destinoPrueba = "";
+
+// Realiza acciones con los datos (por ejemplo, muestra una alerta)
+function recuperarDestino(){
+    debugger;
+}
+
+// Agrega un evento de escucha para el envío del formulario
+// formulario.addEventListener('submit', (event) => {
+//     event.preventDefault(); // Evita que la página se recargue automáticamente al enviar el formulario
+
+//     // Obtén los valores de los campos
+//     const nombre = document.getElementById('nombre').value;
+//     const apellido = document.getElementById('apellido').value;
+//     const fechaVuelo = document.getElementById('fecha_vuelo').value;
+
+//Realiza acciones con los datos (por ejemplo, muestra una alerta)
+alert(`¡Hola, ${nombre} ${apellido}! Tu vuelo está programado para el ${fechaVuelo}.`);
+
+//     // Aquí puedes agregar más lógica según tus necesidades
+//     // Por ejemplo, enviar los datos al servidor o redirigir a otra página
+// });
+
+export default pantallaDestino;
--- js/index.js
+++ js/index.js
@@ -1,27 +1,59 @@
 // se importa destinos al index.
+import pantallaDestino from "./formulario.js";
 import destinos from "./destinos.js";
+
+
+var destino = "";
 
 //funcion para retonar el HTML de una card
 function retornarCardHTML(destino) {
-    return `<div class="card">
-    
-    <img src="${destino.imagen}" alt="${destino.nombre}"></div>
-    <div class="card-name">${destino.nombre}</div>
-    <div class="card-price">${destino.precio}</div>
-    <div class="card-button">
-    <button class="button button-outline button-add" id="${destino.id}" title="Click para elegir destino">Elegir</button>
-    </div>
+    return `<div class=" card">
+        <img src="${destino.imagen}" alt="${destino.nombre}"></div>
+        <div class=" card-price">${destino.precio}</div>
+        <div class=" card-name">${destino.nombre}</div>
+        <div class=" card-button">
+        <button class=" button button-outline button-add" id="${destino.id}" onclick="location.href='formulario.html' title="Click para elegir destino">Elegir</button>
+        </div>
+        </div>
     </div>`
+}
+
+
+function cargarPantallaDestino(destino) {
+    // const container = document.querySelector('.container')
+    //     const cardHTML = retornarCardHTML(destino);
+    //     container.innerHTML += cardHTML;
+    location.href = 'formulario.html';
+    pantallaDestino.destinoPrueba = destino;
+    pantallaDestino.recuperarDestino()
+
+    //pantallaDestino.recuperarDestino();
 }
 
 //funcion para cargar los productos en la pagina
 function CargarDestinos(array) {
     const container = document.querySelector('.container')
-    array.forEach( destinos => {
+    array.forEach(destinos => {
         const cardHTML = retornarCardHTML(destinos);
         container.innerHTML += cardHTML;
     });
 }
 
+const activarClickEnBotones = () => {
+    const botonesAgregar = document.querySelectorAll('.button.button-outline.button-add');
+    if (botonesAgregar) {
+        for (const boton of botonesAgregar) {
+            boton.addEventListener('click', (event) => {
+                cargarPantallaDestino(event.target.id-1);
+            });
+        }
+    }
+};
+
+// Llama a la función para activar los clics en los botones
+
+
 //llamar a la funcion para cargar datos en la pagina
 CargarDestinos(destinos);
+activarClickEnBotones();
+export default destino;
